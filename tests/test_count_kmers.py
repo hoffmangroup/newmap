7fbf89fa18ad249724df905ea40bb8d5d54320bd
Eric Roberts <notthebmovieactor@gmail.com>
2024-07-31 11:34:17 -0400

    Skip tests that rely on an existing index

--- tests/test_count_kmers.py
+++ tests/test_count_kmers.py
@@ -10,17 +10,20 @@ class TestCountKmers(unittest.TestCase):
         self.genome_index_filename = str(TEST_DATA_PATH / 'genome.awfmi')
         self.num_threads = 1
 
+    @unittest.skip("Test relies on large data file not in respository")
     def test_count_kmers(self):
         counts = count_kmers(self.genome_index_filename,
                              [b'AAAA', b'AT', b'TAT', b'CCC', b'NNN', b'TCGT'],
                              self.num_threads)
         self.assertEqual(counts, [9, 3, 1, 8, 0, 0])
 
+    @unittest.skip("Test relies on large data file not in respository")
     def test_count_wrong_type(self):
         with self.assertRaises(TypeError):
             count_kmers(self.genome_index_filename, ["AAAA"],
                         self.num_threads)
 
+    @unittest.skip("Test relies on large data file not in respository")
     def test_empty_byte_string(self):
         with self.assertRaises(ValueError):
             count_kmers(self.genome_index_filename, [b'AAAA', b'', b'TAT'],
